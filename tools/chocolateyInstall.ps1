66f491682ae30d51aa5592c35ca356c253fff089
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2021-09-14 12:08:24 +0200

    Add files via upload

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$packageName = 'altdrag'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/RamonUnch/AltSnap/releases/download/1.47/AltSnap1.47-inst.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url           = $url
+
+  checksum      = '46C8338616A2A9EA07245616A90D511639F9A8222091491CAF9D209BC6FD3346'
+  checksumType  = 'sha256'
+  
+  silentArgs   = '/S'
+
+  softwareName  = 'AltSnap'
+}
+
+Install-ChocolateyPackage @packageArgs
