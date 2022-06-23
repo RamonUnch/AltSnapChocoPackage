c7adb27425eff9263088182762d3a4e4c776650b
RamonUnch <74856804+RamonUnch@users.noreply.github.com>
2022-06-23 17:41:34 +0200

    Update to v1.53

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName = 'altsnap'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/RamonUnch/AltSnap/releases/download/1.52/AltSnap1.52-inst.exe'
-$url64      = 'https://github.com/RamonUnch/AltSnap/releases/download/1.52/AltSnap1.52-x64-inst.exe'
+$url        = 'https://github.com/RamonUnch/AltSnap/releases/download/1.53/AltSnap1.53-inst.exe'
+$url64      = 'https://github.com/RamonUnch/AltSnap/releases/download/1.53/AltSnap1.53-x64-inst.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,9 +12,9 @@ $packageArgs = @{
 
   url           = $url
   url64bit      = $url64
-  checksum      = '0c573f271c260dc49ad7dfee1564fbdc42cee0df62f08162742e6db0eccfba82'
+  checksum      = 'c9786a05ede6c0a9ecb386d3b0e46da54e7c53905689d678cc8051873fe94a5e'
   checksumType  = 'sha256'
-  checksum64    = '4004b2bdfd705a51c4b5a982e72a35e069f2bcfaa22a85ccafbd10d6a6debfe8'
+  checksum64    = '941fbc752f71d0af597c19f05feb744b5a058cfaeb1f65b5514612c47cded119'
   checksumType64= 'sha256'
 
   silentArgs   = '/S'
